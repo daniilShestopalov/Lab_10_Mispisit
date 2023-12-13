7cba04c7e3bc9a0bbaf7501e5b6a851230e6918c
daniilShestopalov <danya.shestopalov@yandex.ru>
2023-12-13 10:25:17 +0300

    Final code

---
+++ AdministrativeEmployee.java
@@ -0,0 +1,21 @@
+public class AdministrativeEmployee extends Employee {
+
+    public AdministrativeEmployee(String name) {
+        this.name = name;
+    }
+
+    public void checkEquipment() {
+        System.out.println("Оборудование исправно");
+    }
+
+    public AdministrativeEmployee() {
+    }
+
+    @Override
+    public String
+    toString() {
+        return "AdministrativeEmployee{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ Employee.java
@@ -0,0 +1,11 @@
+
+public abstract class Employee {
+
+
+	public int ssNo;
+	public float workExperience;
+	public String name;
+	public String email;
+	public int counter;
+
+}
